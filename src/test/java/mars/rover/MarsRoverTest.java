82e355498b53b21caf32d0b4c6050a5b497b936e
temporaS <lasepans@gmail.com>
2020-06-18 19:35:57 +0200

    unit-test

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -20,4 +20,74 @@ class MarsRoverTest {
         assertEquals("5 1 E", newPosition);
     }
 
+    @Test
+    public void
+    acceptance_test_3() {
+        String newPosition = MarsRover.move(3, 3, 'S', "M");
+        assertEquals("3 2 S", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_4() {
+        String newPosition = MarsRover.move(3, 3, 'W', "M");
+        assertEquals("2 3 W", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_5() {
+        String newPosition = MarsRover.move(3, 3, 'E', "M");
+        assertEquals("4 3 E", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_6() {
+        String newPosition = MarsRover.move(3, 3, 'N', "M");
+        assertEquals("3 4 N", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_7() {
+        String newPosition = MarsRover.move(3, 3, 'S', "LLLLM");
+        assertEquals("3 2 S", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_8() {
+        String newPosition = MarsRover.move(3, 3, 'W', "RRRRM");
+        assertEquals("2 3 W", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_9() {
+        String newPosition = MarsRover.move(3, 3, 'E', "LLLLM");
+        assertEquals("4 3 E", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_10() {
+        String newPosition = MarsRover.move(3, 3, 'N', "RRRRM");
+        assertEquals("3 4 N", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_11() {
+        String newPosition = MarsRover.move(3, 3, 'S', "LM");
+        assertEquals("4 3 E", newPosition);
+    }
+
+    @Test
+    public void
+    acceptance_test_12() {
+        String newPosition = MarsRover.move(3, 3, 'N', "RM");
+        assertEquals("4 3 E", newPosition);
+    }
+
 }
